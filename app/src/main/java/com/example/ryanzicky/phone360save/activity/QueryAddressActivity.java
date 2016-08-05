9c9700c55ad03960eb0cca6d1478809352ac4335
ryanzicky <ruiz3431@163.com>
2016-08-05 20:03:59 +0800

    更新代码

--- app/src/main/java/com/example/ryanzicky/phone360save/activity/QueryAddressActivity.java
+++ app/src/main/java/com/example/ryanzicky/phone360save/activity/QueryAddressActivity.java
@@ -2,6 +2,14 @@ package com.example.ryanzicky.phone360save.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.ryanzicky.phone360save.R;
 import com.example.ryanzicky.phone360save.engine.AddressDao;
@@ -10,12 +18,73 @@ import com.example.ryanzicky.phone360save.engine.AddressDao;
  * Created by ryanzicky on 2016/8/5.
  */
 public class QueryAddressActivity extends Activity{
+    private EditText et_phone;
+    private Button bt_query;
+    private TextView tv_query_result;
+    private String mAddress;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            //4.控件使用查询结果
+            tv_query_result.setText(mAddress);
+        };
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_address);
 
-        //测试代码，测试查询引擎类是否成功
-        AddressDao.getAddress("13000201234");
+        initUI();
+    }
+
+    private void initUI() {
+        et_phone = (EditText) findViewById(R.id.et_phone);
+        bt_query = (Button) findViewById(R.id.bt_query);
+        tv_query_result = (TextView) findViewById(R.id.tv_query_result);
+
+        //1.点击查询功能，注册按钮的点击事件
+        bt_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = et_phone.getText().toString();
+                //2.查询是耗时操作，开启子线程
+                query(phone);
+            }
+        });
+
+        //5.实时查询（监听输入框变化）
+        et_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String phone = et_phone.getText().toString();
+                query(phone);
+            }
+        });
+    }
+
+    /**
+     * 获取电话号码归属地
+     * @param phone 查询的电话号码
+     */
+    protected void query(final String phone) {
+        new Thread(){
+            @Override
+            public void run() {
+                mAddress  = AddressDao.getAddress(phone);
+                //3.消息机制，告知主线程查询结束，可以去使用查询结果
+                mHandler.sendEmptyMessage(0);
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/ryanzicky/phone360save/engine/AddressDao.java
+++ app/src/main/java/com/example/ryanzicky/phone360save/engine/AddressDao.java
@@ -12,29 +12,77 @@ public class AddressDao {
     //1.指定访问数据库的路径
     public static String path = "data/data/com.example.ryanzicky.phone360save/files/address.db";
     private static final String tag = "AddressDao";
+    private static String mAddress;
 
     /**
      * 传递一个电话号码，开启数据库连接，进行访问，返回一个归属地
      * @param phone 查询电话号码
      */
-    public static void getAddress(String phone){
-        phone = phone.substring(0, 7);
+    public static String getAddress(String phone){
+        //正则表达式，匹配手机号
+        //手机号码的正则表达式
+        String regularExpression = "^1[3-8]\\d{9}";
         //2.开启数据库连接
         SQLiteDatabase db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
-        //3.数据库查询
-        Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
-        //4.查到即可
-        if(cursor.moveToNext()){
-            String outkey = cursor.getString(0);
-            Log.i(tag,"outkey = " + outkey);
+        if(phone.matches(regularExpression)){
+            phone = phone.substring(0, 7);
+            //3.数据库查询
+            Cursor cursor = db.query("data1", new String[]{"outkey"}, "id = ?", new String[]{phone}, null, null, null);
+            //4.查到即可
+            if(cursor.moveToNext()){
+                String outkey = cursor.getString(0);
+                Log.i(tag,"outkey = " + outkey);
 
-            //5.通过data1查询到的结果，作为外键查询data2
-            Cursor indexCursor = db.query("data2", new String[]{"location"}, "id = ?", new String[]{outkey}, null, null, null);
-            if(indexCursor.moveToNext()){
-                //6.获取查询到的电话归属地
-                String address = indexCursor.getString(0);
-                Log.i(tag,"address = " + address);
+                //5.通过data1查询到的结果，作为外键查询data2
+                Cursor indexCursor = db.query("data2", new String[]{"location"}, "id = ?", new String[]{outkey}, null, null, null);
+                if(indexCursor.moveToNext()){
+                    //6.获取查询到的电话归属地
+                    mAddress = indexCursor.getString(0);
+                    Log.i(tag,"address = " + mAddress);
+                }else {
+                    mAddress = "未知号码";
+                }
+            }else{
+                int length = phone.length();
+                switch (length){
+                    case 3:
+                        mAddress = "报警电话";
+                        break;
+                    case 4:
+                        mAddress = "模拟器";
+                        break;
+                    case 5:
+                        mAddress = "服务电话";
+                        break;
+                    case 7:
+                        mAddress = "本地电话";
+                        break;
+                    case 8:
+                        mAddress = "本地电话";
+                        break;
+                    case 11:
+                        //(3+8)区号+座机号码（外地）,查询data2
+                        String area = phone.substring(1, 3);
+                        Cursor cursor1 = db.query("data2", new String[]{"location"}, "area = ?", new String[]{area}, null, null, null);
+                        if(cursor1.moveToNext()){
+                            mAddress = cursor1.getString(0);
+                        }else{
+                            mAddress = "未知号码";
+                        }
+                        break;
+                    case 12:
+                        //(4+8)区号（0713（湖北黄冈））+座机号码（外地）
+                        String area1 = phone.substring(1, 4);
+                        Cursor cursor2 = db.query("data2", new String[]{"location"}, "area = ?", new String[]{area1}, null, null, null);
+                        if(cursor2.moveToNext()){
+                            mAddress = cursor2.getString(0);
+                        }else{
+                            mAddress = "未知号码";
+                        }
+                        break;
+                }
             }
         }
+        return mAddress;
     }
 }
